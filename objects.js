4eca7a70ec84a1f65a3cb8526cf7e2a27e1bab4a
ShahidSolver <108408906+ShahidSolver@users.noreply.github.com>
2022-08-01 16:30:46 +0500

    Add files via upload

--- objects.js
+++ objects.js
@@ -2,20 +2,33 @@
 // const data = ["shahid", "software", 2022 - 1998, ["ali", "qasim", "asad"]];
 // console.log(data);
 // In arrays we can acces elements/varaibles in an order
-//but for unorder list etc  we used  objects which can acces every element or variable with object-properies
+// but for unorder list etc  we used  objects which can acces every element
+// or variable with object-properies
 
 console.log("WE are going to start Objects");
 //Object Decalaration
 const data = {
-  firstName: "shahid",
+  firstName: "Shahid",
   lastName: "boss",
   occupation: "software",
-  age: 2022 - 1998,
+  birth: 1998,
   friends: ["ali", "qasim", "asad"],
+  ageCalc: function () {
+    this.aged = 2022 - this.birth;
+    return this.aged;
+  },
+  hasDriverLicense: false,
+  driver: function () {
+    if (this.hasDriverLicense == true) {
+      return "has a Drivers License";
+    } else {
+      return "Doesn't has a Drivers Licence";
+    }
+  },
 };
 //Just show same as Array
 console.log(data);
-//but in objects i we want to acces properties of Objects we use
+//but in objects we want to acces properties of Objects we use
 console.log(data.firstName);
 console.log(data.lastName);
 // to output complete name
@@ -25,10 +38,72 @@ console.log("with another way");
 const nameKey = "Name";
 
 //console.log(data.first + nameKey);
-// we can't do with the above method for this we have to represent is like a array
+// we can't do with the above method for this we have to represent is like an array
 console.log(data["first" + nameKey]);
 console.log(data["last" + nameKey]);
 
 //To enter value from user from prompt box
-const input prompt(`Choose from below
+console.log("/////Prompt Box//////");
+const input = prompt(`Choose from below
  firstName : lastName : occupation : age : friends `);
+if (data[input]) {
+  console.log(data[input]);
+} else {
+  prompt(`inValid Input choose from given below
+  firstName : lastName : occupation : age : friends `);
+}
+
+//Chalange
+console.log("///Challange///");
+console.log(
+  `${data.firstName} has ${data.friends.length} friends but ${data.friends[0]} is his best friend`
+);
+
+console.log(data.ageCalc(1998));
+console.log(data["ageCalc"](1998));
+console.log("After Changing it to this");
+console.log(data.ageCalc());
+
+function getSummary() {
+  console.log(`Summary of this person:
+   ${data.firstName} is : `);
+  return console.log(
+    `${
+      data.firstName
+    } is a ${data.ageCalc()} years old teacher ${data.driver()}`
+  );
+}
+console.log(getSummary());
+
+//Coding Challange Begins Here
+
+console.log("The New Challange");
+
+// Bmi = mass / height *height
+
+const markObj = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBmi: function () {
+    const Bmi = this.mass / (this.height * this.height);
+    return Bmi;
+  },
+};
+const johnObj = {
+  fullName: "john Smith",
+  mass: 92,
+  height: 1.95,
+  calcBmi: function () {
+    const Bmi = this.mass / (this.height * this.height);
+    // we can also write the above line with these 2 bellow ways
+    // johnOBj.Bmi = this.mass / (this.height * this.height);
+    // this.Bmi = this.mass / (this.height * this.height);
+    return Bmi;
+  },
+};
+console.log(
+  `${johnObj.fullName} BMI (${johnObj.calcBmi()})'s is higher than ${
+    markObj.fullName
+  }'s BMI (${markObj.calcBmi()}) !! )`
+);
